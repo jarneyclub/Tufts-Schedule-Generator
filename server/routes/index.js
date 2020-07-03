14edf625a9cdb4a8e4cac8e9b97720ceb0fce50a
Jeremy Jung <jangho.j@gmail.com>
2020-07-03 23:44:16 +0900

    simple course catalog API

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,60 @@
 const express = require('express');
 const router = express.Router();
-//look at differnt ways to send data via send and json
+const fs = require('../fs/read_file.js');
 
+/*
+var json_object;
+fs.get_json(function (err,data){
+    json_object = data;
+})*/
+
+
+fs.get_json(function (err,courses_info) {
+    router.get('/', async (req, res) => {
+        var course = req.query.course;
+        var id_names_list = courses_info.courses_id_names;
+        for( var index in id_names_list) {
+            if (id_names_list[index] == course) {
+                console.log(id_names_list[index]);
+                var information = courses_info.Courses[course]
+                console.log(information)
+                res.send(information);
+            }
+        }
+        //get query parameters
+        //res.send(courses_info);
+    });
+})
+
+
+
+/*
 // Do work here
 //example: localhost:7777/?name=jeremy&age=30&cool=false
-router.get('/', (req, res) => {
-  const jer = { name: "jeremy", age: 20, cool: true};
-  //res.send('Hey! It works!');
-  //res.json(jer)
-  //res.send(req.query.name);
+router.get('/', async (req, res) => {
+    var json_object;
+    fs.get_json(function (err,data){
+        //console.log(data);
+        json_object = data;
+    })
 
-  //get query parameters
-  res.send(req.query);
+    const jer = { name: "jeremy", age: 20, cool: true};
+    //res.send('Hey! It works!');
+    //res.json(jer)
+    //res.send(req.query.name);
 
-  //res.render("hello");
-});
+    //get query parameters
+    res.send(req.query);
+
+    //res.render("hello");
+});*/
 
 //putting colon behind will give you a variable on each of your requests
 //example localhost:7777/reverse/jeremy
 router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join()
-  //req.params to access things in the URL
-  //res.send(req.params.name);
-  res.send(reverse)
+    const reverse = [...req.params.name].reverse().join()
+    //req.params to access things in the URL
+    //res.send(req.params.name);
+    res.send(reverse)
 })
 module.exports = router;
