13bb9101c86f75cdac153fb31d05ee726bdc94da
Jeremy Jung <jangho.j@gmail.com>
2020-10-25 19:19:49 -0400

    Update read_file.js

--- routes/fs/read_file.js
+++ routes/fs/read_file.js
@@ -5,26 +5,18 @@ const path = require('path');
 *  Call callback function with parsed JSON
 */
 module.exports.get_json = function get_json(callback) {
-    var data = fs.readFileSync(path.join(__dirname, '/scrape/data.txt'), 'utf8');
-    
-    // parse course catalog into JSON object
-    var courses_json;
-    courses_json = JSON.parse(data);
+    fs.readFile(path.join(__dirname, '/scrape/data.txt'), 'utf8', function (err, data) {
+        // callback with error
+        if (err) return callback(err);
 
-    callback(null, courses_json);
+        console.log("running get_json");
 
-    // fs.readFile(path.join(__dirname, '/scrape/data.txt'), 'utf8', function (err, data) {
-        //callback with error
-        // if (err) return callback(err);
-// 
-        // console.log("running get_json");
-// 
-       // parse course catalog into JSON object
-        // var courses_json;
-        // courses_json = JSON.parse(data);
-// 
-        // callback(null, courses_json);
-    // })
+        // parse course catalog into JSON object
+        var courses_json;
+        courses_json = JSON.parse(data);
+
+        callback(null, courses_json);
+    })
 }
 
 /*
