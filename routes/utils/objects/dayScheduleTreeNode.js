0fa3436dee5dc89bdf910513027503953b993308
Jeremy Jung <jangho.j@gmail.com>
2020-11-09 15:46:21 -0500

    define augmented tree node

---
+++ routes/utils/objects/dayScheduleTreeNode.js
@@ -0,0 +1,78 @@
+/*
+    Node definition for AVL tree
+    !!!!
+    To be used in tree that represents SECTIONS IN A SECTION TYPE
+    - Not augmented
+    !!!!
+
+    Augmentation:
+    1) list of duplicates
+    2) span of class period
+        span = max{ largest endTime , largest value in the subtree}
+
+    Handling duplicates:
+    - Section's with the same start times are appended in a list
+
+    USAGE:
+    Node Construction:
+        // assume sec is a Section object
+        var newNode = new Node(sec);
+    Node comparison:
+        if (node1.getValue() > node2.getValue())
+    Node duplicate section insertion
+        node.storeObject(sec)
+
+*/
+
+function Node(sectionInput) {
+
+    var value = sectionInput.getStartTime();
+    var span = sectionInput.getEndTime();
+    /* array of duplicates that have the same value */
+    var objects = [];
+
+    function getValue() {
+        return value;
+    }
+
+    /** store any object in objects
+     * @param {any} object 
+     */
+    function storeObject(object) {
+        objects.push(object);
+    }
+
+    /**
+     * O(1)
+     * @returns {Array} array of objects
+     */
+    function getObjects() {
+        return objects;
+    }
+
+    /**
+     * @returns {Boolean} whether there are duplicates or not
+     */
+    function noDuplicates() {
+        if (objects.length == 1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function getSpan() {
+        return span;
+    }
+
+    return {
+        getValue: getValue,
+        storeObject: storeObject,
+        getObjects: getObjects,
+        getSpan: getSpan,
+        noDuplicates: noDuplicates,
+        value: value,
+        objects: objects
+    }
+}
