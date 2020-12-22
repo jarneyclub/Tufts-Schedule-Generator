d33cf539c250f9edc6013a767d8cecfe7fca8ee7
Jeremy Jung <jangho.j@gmail.com>
2020-12-21 22:12:43 -0500

    Tree2 Implementation and Testing

---
+++ routes/utils/objects/classes/Class.js
@@ -0,0 +1,130 @@
+
+/** Class class
+ * Definition: 
+ * - a Class is a single weekly class period of a Section 
+ *   that occurs on the same day, same time, every week.
+* @param {any} inputCourseID
+ * @param {any} inputSectionName
+ * @param {any} inputSectionType
+ * @param {integer} inputDay e.g. Monday = 1
+ * @param {integer} inputStartTime
+ * @param {integer} inputEndTime 
+ * @param {any} inputTimes {0: day of week, 1: [begin, end]}
+ * @param {any} inputLocation
+ * @param {any} inputCity
+ * @param {any} inputInstructors
+ * @returns 
+ */
+function Class(inputCourseID, inputSectionName, inputSectionType, inputDay, inputStartTime, inputEndTime, inputLocation, inputCity, inputInstructors) {
+
+    //////////////////////////////////////////
+    //                                      //
+    //          Global Variables            //
+    //                                      //
+    //////////////////////////////////////////
+
+    const courseID = inputCourseID;
+    const sectionName = inputSectionName;
+    const sectionType = inputSectionType;
+
+    const dayOfWeek = inputDay
+
+    // NOTE: time = 0 is 12AM
+    const timeBegin = inputStartTime;
+    const timeEnd = inputEndTime;
+
+    const location = inputLocation;
+    const city = inputCity;
+    const instructors = inputInstructors;
+
+    //////////////////////////////////////////
+    //                                      //
+    //          Public Functions            //
+    //                                      //
+    //////////////////////////////////////////
+
+    /** Get duration of section
+     * @returns {integer} 1 = a minute
+     */
+    function getDuration() {
+        return timeEnd - timeBegin;
+    }
+
+    /** Get day of the week
+     * @returns {integer} day of the week (e.g. monday = 1 )
+     */
+    function getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    /** Get start time of section
+     * @returns {integer} [0 to 1440], 0 being 12AM, 1440 being 12AM 24 hrs later
+     */
+    function getStartTime() {
+        return timeBegin;
+    }
+
+    /** Get end time of section
+     * @returns {integer} [0 to 1440], 0 being 12AM, 1440 being 12AM 24 hrs later
+     */
+    function getEndTime() {
+        return timeEnd;
+    }
+
+    /** Get instructors of the section
+     * @returns {list} names of instructors
+     */
+    function getInstructors() {
+        return instructors;
+    }
+
+    /** Get courseID of section
+     * @returns {string} course ID (e.g. COMP-0015)
+     */
+    function getCourseID() {
+        return courseID
+    }
+
+    /** Get name of section
+     * @returns {string} section name (e.g. LEC-1)
+     */
+    function getSectionName() {
+        return sectionName
+    }
+
+    /** Get type of section
+     * @returns {string}
+     */
+    function getSectionType() {
+        return sectionType;
+    }
+
+    /** Get location of section
+     * @returns {string} 
+     */
+    function getLocation() {
+        return location;
+    }
+
+    /** Get city of section
+     * @returns {string}
+     */
+    function getCity() {
+        return city;
+    }
+
+    return {
+        getDuration,
+        getDayOfWeek,
+        getStartTime,
+        getEndTime,
+        getInstructors,
+        getCourseID,
+        getSectionName,
+        getLocation,
+        getCity,
+        getSectionType
+    }
+}
+
+module.exports = Class;
---
+++ routes/utils/testTree2.js
@@ -0,0 +1,419 @@
+const Tree2 = require('./Tree2.js');
+const Class = require('./objects/classes/Class.js');
+
+const arrayEquals = (a, b) => {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
+
+const testInsertion = () => {
+    console.log("--- unit test: ( INSERTION ) ---")
+
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 7, 15, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 2, 7, 15, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 3, 7, 15, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "D", "testSec", 4, 7, 15, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "E", "testSec", 5, 7, 15, "test location", "test city", ["test instructor"]))
+
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'B', 'A', 'D', null, null, 'C', 'E']
+    if (arrayEquals(output, answer)) {
+        result = true;
+    }
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end: ( INSERTION ) ---")
+    }
+
+    return result;
+}
+
+const testLeftInsertion = () => {
+    console.log("--- unit test: ( AVL Tree LEFT INSERTION ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "B", "testSec", 1, 500, 700, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "A", "testSec", 1, 200, 400, "test location", "test city", ["test instructor"]))
+
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'B', 'A', null]
+    if (arrayEquals(output, answer)) {
+        result = true;
+    }
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree LEFT INSERTION ) ---")
+    }
+
+    return result;
+}
+
+const testRightInsertion = () => {
+    console.log("--- unit test: ( AVL Tree RIGHT INSERTION ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "B", "testSec", 1, 200, 400, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "A", "testSec", 1, 500, 700, "test location", "test city", ["test instructor"]))
+
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'B', null, 'A']
+    if (arrayEquals(output, answer)) {
+        result = true;
+    }
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree RIGHT INSERTION ) ---")
+    }
+
+    return result;
+}
+
+const testBSTInvariant = () => {
+    console.log("--- unit test: ( AVL Tree BST Invariant ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 500, 700, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 200, 400, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 900, 1100, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'A', 'B', 'C']
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree BST Invariant ) ---")
+    }
+    return result;
+}
+
+const testLeftRotation = () => {
+    console.log("--- unit test: ( AVL Tree LEFT ROTATION ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 100, 150, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 50, 90, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 200, 250, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "D", "testSec", 1, 300, 350, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "E", "testSec", 1, 400, 450, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'A', 'B', 'D', null, null, 'C', 'E']
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree LEFT ROTATION ) ---")
+    }
+    return result;
+}
+
+const testRightLeftRotation = () => {
+    console.log("--- unit test: ( AVL Tree RIGHT,LEFT ROTATION ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 100, 150, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 50, 90, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 200, 250, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "D", "testSec", 1, 400, 450, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "E", "testSec", 1, 300, 350, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'A', 'B', 'E', null, null, 'C', 'D']
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree RIGHT,LEFT ROTATION ) ---")
+    }
+    return result;
+}
+
+const testLeftRightRotation = () => {
+    console.log("--- unit test: ( AVL Tree LEFT,RIGHT ROTATION ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 300, 350, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 200, 250, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 400, 450, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "D", "testSec", 1, 50, 90, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "E", "testSec", 1, 100, 150, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'A', 'E', 'C', 'D', 'B', null, null]
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree LEFT,RIGHT ROTATION ) ---")
+    }
+    return result;
+}
+
+const testLeftRotationRoot = () => {
+    console.log("--- unit test: ( AVL Tree LEFT ROTATION on ROOT ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 300, 350, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 400, 450, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 600, 650, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'B', 'A', 'C']
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree LEFT ROTATION on ROOT ) ---")
+    }
+    return result;
+}
+
+const testRightRotation = () => {
+    console.log("--- unit test: ( AVL Tree RIGHT ROTATION ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 300, 350, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 200, 250, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 400, 450, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "D", "testSec", 1, 100, 150, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "E", "testSec", 1, 50, 90, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'A', 'D', 'C', 'E', 'B', null, null]
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree RIGHT ROTATION ) ---")
+    }
+    return result;
+}
+
+const testRightRotationRoot = () => {
+    console.log("--- unit test: ( AVL Tree RIGHT ROTATION on ROOT ) ---")
+    let result = false;
+    /* INITIALIZE TREE */
+    let tree = new Tree2();
+
+    /* INITIALIZE OBJECTS TO INSERT */
+    let testObjects = [];
+    testObjects.push(new Class("Test", "A", "testSec", 1, 300, 350, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "B", "testSec", 1, 100, 150, "test location", "test city", ["test instructor"]))
+    testObjects.push(new Class("Test", "C", "testSec", 1, 50, 90, "test location", "test city", ["test instructor"]))
+    /* insert sections */
+    for (let index in testObjects) {
+        let object = testObjects[index];
+
+        // Note: insert(object, value, name, augmentation)
+        tree.insert(object);
+    }
+
+    /* END init tree */
+    let output = tree.print("array");
+    let answer = [undefined, 'B', 'C', 'A']
+    if (arrayEquals(output, answer))
+        result = true;
+    else {
+        tree.print("tree");
+        console.log("Failure: ")
+        console.log("Expected print: ", answer);
+        console.log("Output: ", output);
+        console.log("inorder output");
+        tree.print("inorder");
+        console.log("--- end : ( AVL Tree RIGHT ROTATION on ROOT ) ---")
+    }
+    return result;
+}
+
+
+const testTree2 = () => {
+    console.log("### Testing TREE 2 ###");
+    let testResults = [];
+    testResults.push(testInsertion());
+    testResults.push(testLeftInsertion());
+    testResults.push(testRightInsertion());
+    testResults.push(testBSTInvariant());
+    testResults.push(testLeftRotation());
+    testResults.push(testRightLeftRotation());
+    testResults.push(testLeftRightRotation());
+    testResults.push(testLeftRotationRoot());
+    testResults.push(testRightRotation());
+    testResults.push(testRightRotationRoot());
+
+    let testsPass = true;
+    for (let index in testResults) {
+        if (testResults[index] == false) {
+            testsPass = false;
+        }
+    }
+
+    if (testsPass == true)
+        console.log("Tree2 passed all tests")
+    else
+        console.log("Tree2 DID NOT pass all tests")
+
+
+}
+testTree2();
+exports.testTree2 = testTree2;
