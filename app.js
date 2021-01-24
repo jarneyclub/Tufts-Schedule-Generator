b27d8e04744ca41e2ea7b9088c60e0e0e35d2bf1
Jeremy Jung <jangho.j@gmail.com>
2021-01-24 10:18:52 -0500

    UPDATE: Access Control Allow Headers

--- app.js
+++ app.js
@@ -36,10 +36,10 @@ app.use("*",function (req, res, next) {
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', "Content-Type");
-    // res.header(
-      // 'Access-Control-Allow-Headers',
-      // 'access-control-request-headers'
-    // );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'access-control-request-headers'
+    );
     next();
 });
 
