5b4b90e65c59e6dcf1e9116233057e303bda2388
Jeremy Jung <jangho.j@gmail.com>
2021-06-20 13:14:43 -0400

    debug authorization logic

--- controllers/auth/authController.js
+++ controllers/auth/authController.js
@@ -1,15 +1,48 @@
-const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
-    let token = jwt.sign(email, process.env.TOKEN_SECRET);
+    const { userid, password } = req.body;
+    let dbUsers = mongoose.connection.collection("users"); // get MongoDB collection
+    console.log("in here")
+    let result = await dbUsers.findOne({
+        userid: userid
+    });
+    if (result === null)
+        return res.sendStatus(403);
+    
+    let token = jwt.sign({ userid: userid, password: password}, process.env.TOKEN_SECRET, { expiresIn: '24h'});
     res.json({ token });
 };
 
 /* Janith Kasun */
 exports.authenticateToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (token) {
+        jwt.verify(token, process.env.TOKEN_SECRET, async (err, userdata) => {
+            console.log("(authenticateToken) userdata: ", userdata);
+            if (err) {
+                return res.sendStatus(403);
+            }
+            let dbUsers = mongoose.connection.collection("users"); // get MongoDB collection
+            let result = await dbUsers.findOne({
+                userid: userdata.userid
+            });
+            
+            if (result === null)
+                return res.sendStatus(403);
+            
+            req.user_id = userdata.userid;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+// DRopped feature
+// before "registering" a guest, check if guest is already "registered"
+exports.checkGuestToken = async (req, res ,next) => {
     const token = req.headers.authorization;
     if (token) {
         jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
@@ -18,16 +51,38 @@ exports.authenticateToken = async (req, res, next) => {
             }
             let dbUsers = mongoose.connection.collection("users"); // get MongoDB collection
             let result = await dbUsers.findOne({
-                email: user
+                userid: user
             });
-            
-            if (result === null)
+
+            if (result === null) {
+                /* guest's authroization header is incorrect */
                 return res.sendStatus(403);
+            }
+            if (result.guest === false) {
+                /* guest's authorization is for a registered user */
+            }
             
-            req.user_id = user;
-            next();
+            /* a valid guest token was already provided */
+            res.json({ token }); // send token back for future use
         });
     } else {
-        res.sendStatus(401);
+        next();
     }
+}
+
+// Dropped feature
+exports.loginGuest = async (req, res) => {
+    let userid = req.guestUserId;
+
+    let dbUsers = mongoose.connection.collection("users"); // get MongoDB collection
+    let result = await dbUsers.findOne({
+        userid: userid
+    });
+
+    if (result === null)
+        return res.sendStatus(403);
+
+    let token = jwt.sign(userid, process.env.TOKEN_SECRET);
+
+    res.json({ token });
 }
--- controllers/auth/userController.js
+++ controllers/auth/userController.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+// const Guest = mongoose.model('Guest');
 const passport = require('passport');
 const { body, validationResult } = require('express-validator');
 
-exports.validateRegister = async (req, res, next) => {
+exports.validateRegisterLocal = async (req, res, next) => {
 
-    const {name, email, email_confirmation, password} = req.body;
+    const {firstname, lastname, userid, password, password_confirmation} = req.body;
 
     // req.sanitizeBody('name');
     // req.checkBody('name', 'You must enter a name!').notEmpty();
@@ -19,14 +20,9 @@ exports.validateRegister = async (req, res, next) => {
     // });
 
     // check if email addresses match
-    let emailsMatch = (email === email_confirmation);
-    if (!emailsMatch)
-        return res.status(400).json({ error: "Email did not match with confirmation" });
-    // check if email is already registered
-    let dbUsers = mongoose.connection.collection("users"); // get MongoDB collection
-    let user_exists = (await dbUsers.findOne({"email" : email}) !== null)
-    if (user_exists) 
-        return res.status(400).json({ error: "Email was already used" });
+    let pwsMatch = (password === password_confirmation);
+    if (!pwsMatch)
+        return res.status(400).json({ error: "Password did not match with confirmation" });
     // validate result
     const errors = validationResult(req);
     if (!errors.isEmpty())
@@ -36,14 +32,46 @@ exports.validateRegister = async (req, res, next) => {
 
 }
 
-exports.register = async (req, res, next) => {
+exports.registerLocal = async (req, res, next) => {
 
     console.log("in here");
     
-    const user = new User({ email: req.body.email, name: req.body.name });
-
+    const user = new User({ userid: req.body.userid, first_name: req.body.firstname, last_name: req.body.lastname });
+    
+    // register user with encrypted password
     User.register(user, req.body.password, (err, user) => {
 
         next(); // pass to authController.login()
     });
+}
+
+// DRopped feature
+exports.registerGuest = async (req, res, next) => {
+
+    // generate random 5 digit integer string
+    let randomId = (Math.randomId() * 1000).toString();
+    console.log("random guest id: ", randomId);
+    // check if this random id already is used
+    let dbUsers = mongoose.connection.collection("users"); // get MongoDB collection
+    let result = await dbUsers.findOne({
+        userid: randomId
+    });
+    
+    // generate random id until an unused one is found
+    while (result !== null) {
+        randomId = (Math.randomId() * 1000).toString();
+        console.log("random guest id: ", randomId);
+        // check if this random id already is used
+        result = await dbUsers.findOne({
+            userid: randomId
+        });
+    }
+    
+    // save guest account to database
+    const guest = new Guest({ userid: randomId, name: req.body.name });
+    await guest.save();
+
+    req.guestUserId = randomId; // set guest's random id in req for next middleware
+
+    next(); // pass to authController.login()
 }
