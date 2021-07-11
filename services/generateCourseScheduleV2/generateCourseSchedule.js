2a16594e1cf496b65bd7ab0dbe411d88e2da6481
Jeremy Jung <jangho.j@gmail.com>
2021-07-11 18:17:05 -0400

    init generate course schedule version 2

---
+++ services/generateCourseScheduleV2/generateCourseSchedule.js
@@ -0,0 +1,13 @@
+const generateCourseSchedule = async (sched_name, term_course_ids, filter, res) => {
+    // get sections based on term_course_ids
+
+    // generate schedules
+
+    // pick one
+
+    // add schedule to database with given sched_Name
+
+    // respond to request with chosen schedule
+}
+
+module.exports = generateCourseSchedule;
