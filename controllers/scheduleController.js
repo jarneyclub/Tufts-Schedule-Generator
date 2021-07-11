296941370904ef79ee91efc691a50e0dfba66861
Jeremy Jung <jangho.j@gmail.com>
2021-07-11 18:17:56 -0400

    init schedule controller

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,3 +1,5 @@
+const generate = require('../services/generateCourseScheduleV2/generateCourseSchedule.js');
 exports.generateSchedule = (req, res) => {
-    
+    const {sched_name, term_course_ids, filter} = req.body;
+    generate(sched_name, term_course_ids, filter, res);
 }
