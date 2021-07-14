c7715a80821daabec2b7633e8986fbf93fa69e3c
Jeremy Jung <jangho.j@gmail.com>
2021-07-13 20:26:02 -0400

    serve from 3000

--- start.js
+++ start.js
@@ -23,24 +23,23 @@ require('./models/external/DegreeReqPublic');
 require('./models/external/Schedule');
 // start app
 const app = require('./app');
-// app.set('port', process.env.PORT || 7777);
-// const server = app.listen(app.get('port'), () => {
-//     console.log(`Express running → PORT ${server.address().port}`);
-// });
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+});
 // console.log(process.env.SSL_PKEY.replace(/\\n/gm, '\n'))
 // console.log(process.env.CSR.replace(/\\n/gm, '\n'))
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer({
-    // key : fs.readFileSync(path.resolve(__dirname, 'routes/generated-private-key.txt')),
-    // cert: fs.readFileSync(path.resolve(__dirname, 'routes/generated-csr.txt'))
-    key: process.env.SSL_PKEY.replace(/\\n/gm, '\n'),
-    cert: process.env.CERT.replace(/\\n/gm, '\n'),
-}, app);
-
-httpServer.listen(80, () => {
-    console.log('HTTP Server running on port 80');
-});
-
-httpsServer.listen(443, () => {
-    console.log('HTTPS Server running on port 443');
-});
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer({
+    // key: process.env.SSL_PKEY.replace(/\\n/gm, '\n'),
+    // cert: process.env.CERT.replace(/\\n/gm, '\n'),
+// }, app);
+// 
+// httpServer.listen(80, () => {
+    // console.log('HTTP Server running on port 80');
+// });
+// 
+// httpsServer.listen(443, () => {
+    // console.log('HTTPS Server running on port 443');
+// });
+// 
