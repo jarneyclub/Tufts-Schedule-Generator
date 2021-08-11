97080f9795cdc2ff0e9d50f7918cf26b01376396
Duncan <19duncanc@tas.tw>
2021-08-10 22:05:26 -0400

    test with 404 error

--- frontend/src/components/reusable/DegreeReqEdit.js
+++ frontend/src/components/reusable/DegreeReqEdit.js
@@ -178,7 +178,7 @@ function DegreeReqEdit(props) {
     await fetch("https://jarney.club/api/degreereqs/private", requestOption)
       .then((response) => {
         console.log("response from fetchCreate: ", response);
-        return response.json();
+        return response.text();
       })
       .then((result) => {
         console.log("result from fetchCreate: ", result);
