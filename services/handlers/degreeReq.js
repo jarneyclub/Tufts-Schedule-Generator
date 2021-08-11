9f0e15a55e1a43d0b732b95873c57fd5661fb05b
Jeremy Jung <jangho.j@gmail.com>
2021-08-10 22:52:57 -0400

    return empty array when no degree req found

--- services/handlers/degreeReq.js
+++ services/handlers/degreeReq.js
@@ -27,6 +27,7 @@ exports.createDegreeReqPublic = async (schema) => {
             if (e.message.indexOf("validation failed") > -1) {
                 /* error is mongoose validation error */
                 throw { code: 1, message: e.message };
+                // throw { id: "201", status: "404", title: "Degree Plan Error", detail: e.message };
             }
             else {
                 console.error(e);
@@ -176,9 +177,6 @@ exports.getDegreeReqsPrivate = async (query) => {
             };
             documents.push(docParsed);
         });
-
-        if (documents.length === 0)
-            throw "No private degree requirement could be found with given user_id.";
 
         return documents
 
@@ -352,5 +350,29 @@ exports.deleteDegreeReqPrivate = async (query) => {
     catch (e) {
         console.error(e);
         throw { code: 4, message: e };
+    }
+}
+
+const errorHandler = (res, err, start) => {
+    console.error("(errorHandler): ", err);
+    switch (err.code) {
+        // 1, when a query/schema is incorrect
+        // 2, when a document was not found in the database
+        case 1:
+        case 2:
+            res.status(400);
+            res.json({
+                err: err.message,
+                time_taken: ((Date.now() - start).toString() + "ms")
+            });
+            break;
+        // 4, a backend error.
+        case 4:
+            res.status(500);
+            res.json({
+                err: "Something went wrong. Requested service is not available at this time. Please notify the backend.",
+                time_taken: ((Date.now() - start).toString() + "ms")
+            });
+            break;
     }
 }
