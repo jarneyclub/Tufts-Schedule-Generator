0aa5a90db1b5e8deaea04aba23ea4241c488f608
Duncan <19duncanc@tas.tw>
2021-08-11 23:21:13 -0400

    json fix

--- frontend/src/components/DegreePlan1.js
+++ frontend/src/components/DegreePlan1.js
@@ -90,21 +90,21 @@ function AddMajorMinor(props) {
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 const degreeReqDefault = {
-  program_name: "Requirement #1",
-  school: "",
-  degree: "",
-  part_id_tracker: 1,
-  parts: [
+  "program_name": "Requirement #1",
+  "school": "",
+  "degree": "",
+  "part_id_tracker": 1,
+  "parts": [
     {
-      part_id: 0,
-      part_name: "",
-      part_desc: "",
-      // part_req_id_tracker: 1,
-      part_reqs: [
+      "part_id": 0,
+      "part_name": "",
+      "part_desc": "",
+      "part_req_id_tracker": 1,
+      "part_reqs": [
         {
-          part_req_id: 0,
-          course_num: "",
-          course_note: "",
+          "part_req_id": 0,
+          "course_num": "",
+          "course_note": "",
         },
       ],
     },
