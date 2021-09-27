5d76524c6a771eac03153c81f811ec60603d7703
Duncan <19duncanc@tas.tw>
2021-09-27 01:39:30 -0400

    css update

--- frontend/src/components/Scheduler.js
+++ frontend/src/components/Scheduler.js
@@ -465,7 +465,7 @@ function Scheduler(props) {
 
         <div className={sStyle.rightColumnWrapper}>
           <div className={sStyle.scheduleTitleContainer}>
-            <div />
+            <div className={sStyle.scheduleSelectorContainer}/>
             <Dropdown
               options={scheduleOptions}
               selectedOption={selectedSchedule}
