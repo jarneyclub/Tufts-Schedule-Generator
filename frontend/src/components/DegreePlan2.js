c886fe569e475243ef015385413706bf8a0798e8
Duncan <56744224+duncanc19116256@users.noreply.github.com>
2021-09-29 01:42:58 -0400

    test

--- frontend/src/components/DegreePlan2.js
+++ frontend/src/components/DegreePlan2.js
@@ -176,12 +176,12 @@ function DegreePlan2(props) {
     console.log("semesterPlanChange e:", e);
     setSelectedPlanID(handleSelectedPlanNameToID(e.target.value));
     console.log("e.target.value", e.target.value);
-    console.log("check: ", semesterPlanOptions[1].plan_name === e.target.value)
+    console.log("check: ", semesterPlanOptions[1].plan_name === e.target.value.substr(3))
     console.log("semesterPlanOptions ", semesterPlanOptions)
     console.log("before substr", e.target.value)
-    console.log("after substr", e.target.value.substr(4));
-    console.log(semesterPlanOptions.findIndex(plan => plan.plan_name === e.target.value.substr(4)))
-    setCardOptions(semesterPlanOptions.find( ({ plan_name }) => plan_name === e.target.value.substr(4) ))
+    console.log("after substr", e.target.value.substr(3));
+    console.log(semesterPlanOptions.findIndex(plan => plan.plan_name === e.target.value.substr(3)))
+    setCardOptions(semesterPlanOptions.find( ({ plan_name }) => plan_name === e.target.value.substr(3) ))
   };
 
   const handleRemoveCards = (cardsToRemove) => {
