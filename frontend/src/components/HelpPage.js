e36dac683b82bdebb89bbf7db2e1e3be8c58e7b5
carysklt <56458748+carysklt@users.noreply.github.com>
2021-10-03 16:44:46 -0400

    help page

--- frontend/src/components/HelpPage.js
+++ frontend/src/components/HelpPage.js
@@ -5,50 +5,75 @@
  * has a feedback form.
  */
 
-import { Accordion, AccordionSummary, AccordionDetails, Typography } from "@material-ui/core";
+import MuiAccordion from '@material-ui/core/Accordion';
+import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
+import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
+import {withStyles} from '@material-ui/core/styles';
 import  ExpandMoreIcon  from "@material-ui/icons/ExpandMore";
 
 import hStyle from "./style/HelpPage.module.css";
-import feedbackJumbo from "./res/feedback_animated.mp4";
+import feedbackJumbo from "./res/feedback.png";
+
+const Accordion = withStyles({
+    root:{
+        backgroundColor: (0,0,0,0.5),
+    },
+    expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+    root:{
+        backgroundColor: (0,0,0,0.125),
+    },
+    expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles({
+    root:{
+        
+    },
+    expanded: {},
+})(MuiAccordionDetails);
 
 function HelpPage () {
     return (
-        <div className={hStyle.verticalContainer}>
+        <div className={hStyle.bodyContainer}>
             <div className={hStyle.horizontalContainer}>
                 <h1>Frequently Asked Questions</h1>
                 <Accordion>
                     <AccordionSummary
                         expandIcon = {<ExpandMoreIcon />}
                     >
-                        <Typography>Accordion 1</Typography>
+                        How do I make my own degree?
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>test</Typography>
+                        test
                     </AccordionDetails>
                 </Accordion>
                 <Accordion>
                     <AccordionSummary
                         expandIcon = {<ExpandMoreIcon />}
                     >
-                        <Typography>Accordion 2</Typography>
+                        Accordion 2
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>test</Typography>
+                        test
                     </AccordionDetails>
                 </Accordion>
                 <Accordion>
                     <AccordionSummary
                         expandIcon= {<ExpandMoreIcon />}
                     >
-                        <Typography>Accordion 3</Typography>
+                        Accordion 3
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography>test</Typography>
+                        test
                     </AccordionDetails>
                 </Accordion>
                 <p></p>
             </div>
             <div className={hStyle.horizontalContainer}>
+                <h1>Contact us!</h1>
                 <div className={hStyle.formContainer}>
                     <form id="contactForm" className={hStyle.contactForm}>
                         <input type="text" name="Name" placeholder="Name"required />
@@ -56,6 +81,9 @@ function HelpPage () {
                         <textarea name="msg" placeholder="Message" required></textarea>
                         <input type="button" class={hStyle.contactForm.submit} name="submit" value="Submit"/>
                     </form>
+                </div>
+                <div className={hStyle.imgContainer}>
+                    <img src={feedbackJumbo} alt="feedbackJumbo"/>
                 </div>
             </div>
         </div>
