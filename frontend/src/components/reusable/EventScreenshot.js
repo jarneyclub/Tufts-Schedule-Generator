3f684cece9173673be08bf470d1162c7ddd29ef5
Duncan <56744224+duncanc19116256@users.noreply.github.com>
2021-10-21 19:22:50 -0400

    eventscreenshot update

--- frontend/src/components/reusable/EventScreenshot.js
+++ frontend/src/components/reusable/EventScreenshot.js
@@ -8,30 +8,52 @@
  */
 
 import { useEffect, useState } from "react";
-import eStyle from "./reusableStyles/Event.module.css";
+import eStyle from "./reusableStyles/EventScreenshot.module.css";
 
-const columnTitles = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Time Unspecified"]
+const columnTitles = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "TimeUnspecified"]
 
 
 function Class(props) {
+  const { classDetail } = props;
+  const { name, details, location, instructors, time_start, time_end} = classDetail;
+
+
+  return (
+    <div className={eStyle.classContainer}>
+      <div>{time_start}~{time_end}</div>
+      <div>{details}</div>
+      <div>{name}</div>
+      <div>{location}</div>
+    </div>
+  )
+
 
 }
 
 
 function EventScreenshot(props) {
-  const { classDetails } = props;
+  const { classDetails, onClose } = props;
 
 
-  const [customStyle, setCustomStyle] = useState({});
+ 
   console.log("eventScreenshot:", classDetails);
   
 
 
   return (
-    <div
-      className={eStyle.eventContainer}
-    >
-      check
+    <div className={eStyle.rowContainer}>
+      {
+        columnTitles.map((title) => (
+          <div className={eStyle.eventsContainer}>
+            <div>{title}</div>
+            {
+              classDetails?.title?.map((details) => (
+                <Class classDetail={details}/>
+              ))
+            }
+          </div>
+        ))
+      }
     </div>
   );
 }
