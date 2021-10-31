35f8fa5be4c5cf6aaa58bbc5f0fe2a48388ce827
Duncan <56744224+duncanc19116256@users.noreply.github.com>
2021-10-31 02:35:44 -0400

    test

--- frontend/src/components/reusable/CourseSearchBar.js
+++ frontend/src/components/reusable/CourseSearchBar.js
@@ -43,7 +43,8 @@ function CourseSearchBar(props) {
   } = props;
   const { course_num, course_title, units_esti } = courseDetail;
 
-  const handleDragStart = (touch) => {
+  const handleDragStart = (e, touch) => {
+    e.preventDefault();
     onTransferCourse && onTransferCourse(courseDetail, touch);
     if (origin !== "courseList") {
       handleCardOrigin(origin);
@@ -68,8 +69,9 @@ function CourseSearchBar(props) {
     <div
       className={cStyle.barContainer}
       draggable={draggable}
-      onTouchStart={() => handleDragStart(true)}
-      onDragStart={handleDragStart}
+      onTouchStart={(e) => handleDragStart(e, true)}
+      onDragStart={(e) => handleDragStart(e, false)}
+      onMouseDown={(e) => handleDragStart(e, true)}
       id={course_num?.concat(course_title)}
       onDoubleClick={handleDoubleClick}
       onClick={handleOnClick}
