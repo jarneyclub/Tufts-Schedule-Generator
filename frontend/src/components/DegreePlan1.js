1c0e576cef5617d5a0475a41d883d4b1bddcc062
Duncan <56744224+duncanc19116256@users.noreply.github.com>
2021-11-07 13:02:08 -0500

    test

--- frontend/src/components/DegreePlan1.js
+++ frontend/src/components/DegreePlan1.js
@@ -360,7 +360,7 @@ function DegreePlan1(props) {
                             display the info of the selected degree plan */}
           <div className={dp1Style.degreeReqListExpandable}>
             <DegreeReqDisplay
-              reqDetail={degreeReqOptions[selectedDegreeReqIdx]}
+              reqDetail={degreeReqOptions && degreeReqOptions[selectedDegreeReqIdx]}
             />
           </div>
 
