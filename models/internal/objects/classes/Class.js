1c4305287eb1738697edc93aaa8b07f29ce0a805
pmelga01 <79951993+pmelga01@users.noreply.github.com>
2022-04-02 11:38:47 -0400

    Changes to ScheduleV2 Schema and Class Object
    Add extra function to check whether time is unspecified for a given class object, and change new schedule schema to expect an attribute with an array of term_section_ids instead of the events

--- models/internal/objects/classes/Class.js
+++ models/internal/objects/classes/Class.js
@@ -138,6 +138,17 @@ function Class(inputCourseID, inputCourseName, inputSectionName, inputSectionTyp
     function getCourseDatabaseId () {
         return courseDatabaseId;
     }
+    
+    /** Checks whether class time is specified (true) or unspecified (false)
+    * @returns {boolean}
+    */
+    function isTimeUnspecified() {
+        if ( (inputStartTime === -1) && (inputEndTime === -1) && (inputDay === -1) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     return {
         getDuration,
@@ -153,6 +164,7 @@ function Class(inputCourseID, inputCourseName, inputSectionName, inputSectionTyp
         getSectionType,
         getSectionID,
         getCourseDatabaseId
+        isTimeUnspecified
     }
 }
 
