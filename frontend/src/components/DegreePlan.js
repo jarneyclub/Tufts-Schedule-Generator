09b694c609699fd763b86e867d3f5d5ba92adcd7
KY233466 <2232201309@qq.com>
2022-04-03 12:22:51 -0400

    Degree Plan
    - remove old course specification

--- frontend/src/components/DegreePlan.js
+++ frontend/src/components/DegreePlan.js
@@ -686,13 +686,6 @@ function DegreePlan(props) {
                   </div>
                 </div>
   
-                {popup.showCourseInfo && (
-                  <CourseInfoExpress
-                    courseInfo={courseInfo}
-                    onClose={() => handlePopup('showCourseInfo', false)}
-                  />
-                )}
-  
                 {/* Degree Requirment Container */}
                 <DegreeReqExpress />
 
