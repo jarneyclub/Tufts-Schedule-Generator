dbd14c38f5a8a2f03de9c66f705157a5469e965c
KY233466 <81402259+KY233466@users.noreply.github.com>
2022-06-17 15:49:03 -0400

    Revert "time unstated styling fix"

--- frontend/src/components/reusable/CalendarDay.js
+++ frontend/src/components/reusable/CalendarDay.js
@@ -152,17 +152,15 @@ function CalendarDay(props) {
       )}
       {/* Generate the time slots for just one column if it is for time unstated */}
       {dayName === "TimeUnspecified" ? (
-        <div className={cStyle.unStatedTimeColumn}>
+        <div className={cStyle.unStatedTimeColumn} > 
           {classesDay?.map((course) => {
-            return (
-              <Button
-                className={sStyle.tuButton}
-                // onClick={() => handleShowCourseInfo(course)}
-              >
-                {course.details}
-              </Button>
-            );
-          })}
+                return (<Button
+                  className={sStyle.tuButton}
+                  // onClick={() => handleShowCourseInfo(course)}
+                >
+                  {course.details}
+                </Button>);
+            })}
         </div>
       ) : (
         <>
@@ -215,19 +213,6 @@ function CalendarDay(props) {
       )}
 
       {/* Put the classes on depending on the day */}
-      {!dayName === "TimeUnspecified"
-        ? classesDay?.map((event) => {
-            return (
-              <Event
-                shrink={shrink}
-                eventDetails={event}
-                onEventClick={onEventClick}
-              ></Event>
-            );
-          })
-        : {}}
-
-      {/* Put the classes on depending on the day
       {dayName === "TimeUnspecified"
         ? classesDay?.map((course) => {
             return (
@@ -247,7 +232,7 @@ function CalendarDay(props) {
                 onEventClick={onEventClick}
               ></Event>
             );
-          })} */}
+          })}
     </div>
   );
 }
