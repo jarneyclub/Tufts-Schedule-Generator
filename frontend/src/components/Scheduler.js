c215547c100c8e6e57bdeca851309ad7c15dd60a
KY233466 <2232201309@qq.com>
2022-07-08 19:42:44 -0400

    course list
    - color change

--- frontend/src/components/Scheduler.js
+++ frontend/src/components/Scheduler.js
@@ -701,7 +701,7 @@ function Scheduler(props) {
                         customStyle={{
                           border: "none",
                           justifyContent: "space-between",
-                          backgroundColor: "#B4ABF0",
+                          backgroundColor: "#6B818F",
                         }}
                         onClick={handleShowCourseInfo}
                       />
