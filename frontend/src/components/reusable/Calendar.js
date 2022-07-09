a9d9567d992bcd04d55093797c918ab725d8141b
KY233466 <2232201309@qq.com>
2022-07-09 12:52:26 -0400

    shrink

--- frontend/src/components/reusable/Calendar.js
+++ frontend/src/components/reusable/Calendar.js
@@ -62,13 +62,11 @@ function Calendar(props) {
   const WeekdaysController = () => {
     if (TimeUnstated.length > 0 && weekdays.length === 5) {
       weekdays.push("TimeUnspecified");
-      setMaxDaySelection(5);
       console.log("triggered! ", maxDaySelection);
     }
 
     if (TimeUnstated.length === 0) {
       weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
-      setMaxDaySelection(4);
       console.log(maxDaySelection);
     }
   };
@@ -108,14 +106,17 @@ function Calendar(props) {
    *  purpose: controls the display of day schedule on single day view
    */
   const onDayChange = (direction) => {
-    if (daySelection === maxDaySelection && direction === 1) {
+    if (
+      daySelection === (TimeUnstated.length === 0 ? 4 : 5) &&
+      direction === 1
+    ) {
       setDaySelection(0);
       console.log("first ", daySelection);
-      console.log(maxDaySelection);
+      // console.log(maxDaySelection);
     } else if (daySelection === 0 && direction === -1) {
-      setDaySelection(maxDaySelection);
+      setDaySelection(TimeUnstated.length === 0 ? 4 : 5);
       console.log("second", daySelection);
-      console.log(maxDaySelection);
+      // console.log(maxDaySelection);
     } else {
       setDaySelection((prev) => prev + direction);
     }
